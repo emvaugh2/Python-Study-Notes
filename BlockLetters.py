567eb000235b8561754028af356dc33c5a3edacd
Edward Vaughn <131804156+emvaugh2@users.noreply.github.com>
2025-01-08 10:07:28 -0600

    Add files via upload

---
+++ BlockLetters.py
@@ -0,0 +1,13 @@
+# Edward Vaughn
+# I'm a rockstar. Don't play with me. 
+
+first_initial = """
+EEEEE  V   V
+E      V   V
+E      V   V
+EEE    V   V
+E      V   V
+E       V V
+EEEEE    V
+"""
+print(first_initial)
